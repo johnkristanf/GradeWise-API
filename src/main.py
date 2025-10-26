127e6a0506c1e09407d202e570fda2448c75bcc0
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-10-26 11:26:10 +0800

    cors and db connect lifespan

--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from auth.router import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-app.include_router(auth_router, prefix="auth")
+from .config import settings
+from .auth.router import auth_router
+from .database import Database
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Establish database connection on startup
+    await Database.initialize()
+
+    # Close the database connection on shutdown
+    yield
+    await Database.close_all()
+
+
+app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,  # <-- Pass the class, not an instance
+    allow_origins=settings.CLIENT_ORIGIN_URLS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
 
 @app.get("/health")
-def root():
+def check_server_health():
     return {"message": "Server is Healthy"}
