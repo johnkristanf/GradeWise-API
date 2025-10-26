d1dc6e6b747c9728f0b703914e33356b56d3680f
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-10-26 11:24:22 +0800

    add register endpoint

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,9 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio.session import AsyncSession
 
+from .schemas import FetchUser, RegisterUser
+from .service import AuthService
+
+from ..database import Database
+
+auth_service = AuthService()
 auth_router = APIRouter()
 
 
+@auth_router.post("/register", response_model=FetchUser, status_code=201)
+async def register(
+    user: RegisterUser, session: AsyncSession = Depends(Database.get_session)
+):
+    registered_user = await auth_service.register(user, session)
+    return registered_user
+
+
 @auth_router.post("/login", status_code=200)
-def login(user):
-    # Login operation here
-    return user
+def login(user_credentials):
+    authenticated_user = auth_service.login(
+        user_credentials.email, user_credentials.password
+    )
+    return authenticated_user
