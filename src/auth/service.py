bdb881c0a4a7d43ab28557a6ba03a97e173539f2
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-10-26 14:38:01 +0800

    feat: jwt service class

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,27 +1,52 @@
-from typing import Optional
-from fastapi import HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
 from pwdlib import PasswordHash
 from sqlalchemy import select
+from jose import JWTError, jwt
+
+from datetime import datetime, timedelta
+from typing import Optional
+from fastapi import HTTPException, status
+
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from .models import User
+from ..config import settings
+from ..user.models import User
 from .schemas import FetchUser, RegisterUser
-from ..database import Database
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-password_hash = PasswordHash.recommended()
-oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
+class JWTService:
+    def __init__(self, secret_key: str, algorithm: str):
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+
+    def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
+        expire = datetime.now() + (expires_delta or timedelta(minutes=15))
+
+        to_encode = data.copy()
+        to_encode.update({"exp": expire})
+        return jwt.encode(to_encode, self.secret_key, algorithm=self.algorithm)
+
+    def decode_token(self, token: str):
+        try:
+            return jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
+        except JWTError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid or expired token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
 
 
 class AuthService:
+
+    def __init__(self, jwt_service: JWTService):
+        self.jwt_service = jwt_service
+        self.password_hash = PasswordHash.recommended()
+
     def verify_password(self, plain_password, hashed_password):
-        return password_hash.verify(plain_password, hashed_password)
+        return self.password_hash.verify(plain_password, hashed_password)
 
     def hash_password(self, password):
-        return password_hash.hash(password)
+        return self.password_hash.hash(password)
 
     @staticmethod
     async def get_user_by_email(session: AsyncSession, email: str) -> Optional[User]:
@@ -36,7 +61,7 @@ class AuthService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email address is already registered.",
             )
-            
+
         hashed_password = self.hash_password(user.password)
         user_dict = user.model_dump(exclude={"password"})
 
@@ -46,19 +71,19 @@ class AuthService:
         await session.commit()
         await session.refresh(registered_user)
 
-        return FetchUser(
-            id=registered_user.id,
-            first_name=registered_user.first_name,
-            middle_name=registered_user.middle_name,
-            last_name=registered_user.last_name,
-            email=registered_user.email,
-            created_at=registered_user.created_at,
-            updated_at=registered_user.updated_at,
+        return FetchUser.model_validate(registered_user)
+
+    async def login(self, email: str, password: str, session: AsyncSession):
+        user = await self.get_user_by_email(session, email)
+        if not user or not self.verify_password(password, user.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid credentials",
+            )
+
+        access_token = self.jwt_service.create_access_token(
+            data={"sub": user.id, "email": user.email},
+            expires_delta=timedelta(minutes=settings.JWT_TOKEN_EXPIRES_MINUTES),
         )
 
-    async def login(self, email, password):
-        async with Database.get_cursor() as (cursor, conn):
-            await cursor.execute("SELECT * FROM users WHERE email = %s", (email))
-            user = await cursor.fetchone()
-        print("user: ", user)
-        return user
+        return {"message": "Login Successfully", "access_token": access_token}
