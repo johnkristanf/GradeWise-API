b9b83990cdce7f489bafe395448c2a8c3a7859c7
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-10-26 17:11:53 +0800

    feat: google cloud vision and setup open ai client

--- src/essay/router.py
+++ src/essay/router.py
@@ -1,9 +1,14 @@
-from typing import List, Union
+import io
+
+from typing import List
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from openai import OpenAI
-from ..config import settings
+from src.config import settings
+from google.cloud import vision
 
-client = OpenAI()
+openai_client = OpenAI(api_key=settings.OPEN_AI_API_KEY)
+vision_client = vision.ImageAnnotatorClient()
+
 essay_router = APIRouter()
 
 
@@ -11,8 +16,20 @@ essay_router = APIRouter()
 async def grade(files: List[UploadFile] = File(...)):
     if len(files) == 0:
         raise HTTPException(status_code=400, detail="No file uploaded")
-    
+
     for file in files:
         content = await file.read()
-        
+        image = vision.Image(content=content)
+        response = vision_client.document_text_detection(image=image)
+
+        if response.full_text_annotation:
+            text = response.full_text_annotation.text
+            print("Extracted Text:")
+            print(text)
+
+            response = openai_client.responses.create(
+                model="gpt-4.1-mini",
+                input="Write a one-sentence bedtime story about a unicorn.",
+            )
+
     return
