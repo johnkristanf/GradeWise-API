a14944a000da59195963147c47b6ea03ea463769
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-01 14:32:30 +0800

    add sync session

--- src/database.py
+++ src/database.py
@@ -1,10 +1,11 @@
-from typing import AsyncGenerator
-import aiomysql
+from typing import AsyncGenerator, Generator
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-from contextlib import asynccontextmanager
+from contextlib import contextmanager
 from src.config import settings
 
 
@@ -16,13 +17,12 @@ class Database:
     engine = None
     async_session_maker = None
 
-    # DATABASE INITIALIZATION SECTION
-    @classmethod
-    def initialize(cls):
-        cls.create_session()
+    # For ORM (SQLAlchemy sync)
+    sync_engine = None
+    sync_session_maker = None
 
     @classmethod
-    def create_session(cls):
+    def create_async_session(cls):
         database_url = f"mysql+aiomysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_DATABASE}"
 
         cls.engine = create_async_engine(
@@ -39,9 +39,27 @@ class Database:
             expire_on_commit=False,
         )
 
+    @classmethod
+    def create_sync_session(cls):
+        sync_database_url = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_DATABASE}"
+
+        cls.sync_engine = create_engine(
+            sync_database_url,
+            echo=False,
+            pool_size=5,
+            max_overflow=10,
+            pool_pre_ping=True,
+        )
+
+        cls.sync_session_maker = sessionmaker(
+            cls.sync_engine,
+            class_=Session,
+            expire_on_commit=False,
+        )
+
     # GET CONNECTION SECTION
     @classmethod
-    async def get_session(
+    async def get_async_session(
         cls,
     ) -> AsyncGenerator[AsyncSession, None]:
         async with cls.async_session_maker() as session:
@@ -53,9 +71,27 @@ class Database:
             finally:
                 await session.close()
 
+    @classmethod
+    @contextmanager
+    def get_sync_session(cls) -> Generator[Session, None, None]:
+        session = cls.sync_session_maker()
+        try:
+            yield session
+        except Exception:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
     # CLOSE METHODS SECTION
     @classmethod
-    async def close(cls):
-        """Close ORM engine"""
+    async def close_async(cls):
+        """Close async ORM engine"""
         if cls.engine:
             await cls.engine.dispose()
+
+    @classmethod
+    def close_sync(cls):
+        """Close sync ORM engine"""
+        if cls.sync_engine:
+            cls.sync_engine.dispose()
